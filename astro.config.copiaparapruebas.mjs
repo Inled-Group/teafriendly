f2f69702df14be48ade6813f0b3707f3ea491cf8
JGH Inled Group <github@inled.es>
2025-05-30 18:37:23 +0200

    Página terminada

--- astro.config.copiaparapruebas.mjs
+++ astro.config.copiaparapruebas.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://inled-group.github.io',
-    base: '/teafriendly.es',
+    site: 'https://inled-group.github.io/teafriendly/',
+    base: '/teafriendly/',
 
 });
