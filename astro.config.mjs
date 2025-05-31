7aa44ea02413c2d7a1edfa927f2f46b1b248230b
JGH Inled Group <github@inled.es>
2025-05-31 08:32:31 +0200

    A ver si funciona

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://inled-group.github.io/teafriendly/',
-    base: '/teafriendly/',
+    base: '',
 
 });
 // Algo debo cambiar puesto que esto no funciona
